21e3780e5487d3d10724b49866b31e7fb60e936f
Sumukha Pk <sumukhapk46@gmail.com>
2019-01-26 06:13:04 +0530

    Basic  http routing done

--- src/go/Web Server/main/main.go
+++ src/go/Web Server/main/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Bitch</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<h1>Hello world</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound) // Status code 404
+		fmt.Fprint(w, "<h1>Error 404 : Page not found</h1>")
+	}
+}
 
 func main() {
-	fmt.Print("Hello World!")
+	// router := httprouter.New()
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc) // Path and function to go to // Path matching
+	fmt.Print("Serving on port 3000")
+	http.ListenAndServe(":3000", mux)
 }
